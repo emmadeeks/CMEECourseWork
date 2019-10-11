7f702bc7386a99e8aea9d68adee6f4040cf27c8f
Emma Deeks <ead19@imperial.ac.uk>
2019-10-11 12:45:51 +0100

    Added shell practical to git repository:

---
+++ Week1/Code/csvtospace.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Author: Emma Deeks ead19@imperial.ac.uk
+#Script: csvtospace.sh
+#Desc: Converts comma seperated values to space seperated values 
+#saves the output into a seperately file csvtospace_converted.txt 
+#Arguments: 1 -> tab delimited file 
+#Date: Oct 2019 
+
+for f in `ls ../Data/Temperatures/*.csv`; #goes to the directory containing the csv files 
+    do
+        echo "Creating a comma delimited version of ${f} ..."; 
+        a=`echo ${f}| cut -f 3 -d "."`  # cuts the .csv from the end value so instead of .csv.txt it is just .txt. defines a as the new variable
+        echo ${a} 
+        cat ${f} | tr -s "," "\ " >> ..${a}.txt #replaces commas with spaces and outputs it to a text file 
+        echo "Done!";
+    done
