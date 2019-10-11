bb3f182257a76769b54361fef7712e83deb8b973
Emma Deeks <ead19@imperial.ac.uk>
2019-10-11 09:30:34 +0100

    Added UNIX week 1 practicals.

--- Week1/Code/tabtocsv.sh
+++ Week1/Code/tabtocsv.sh
@@ -6,7 +6,7 @@
 #Arguments: 1 -> tab delimited file 
 #Date: Oct 2019 
 
-echo "Creating a comma delimited version of $..."
+echo "Creating a comma delimited version of $1 ..."
 cat $1 | tr -s "\t" "," >> $1.csv
 echo "Done!"
-exit 
+exit
